474aacd4f3ef03e1c90ce6b7a4c9b4dba8e72b4b
Gere_X <52193900+Gere321123@users.noreply.github.com>
2024-09-17 17:30:32 +0200

    loool

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelpConfig} from "./HelpingConfig.s.sol";
+
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        HelpConfig helperConfig = new HelpConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
-    address testAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     function setUp() external {
-        fundMe = new FundMe(testAddress);
+        DeployFundMe dployFundMe = new DeployFundMe();
+        fundMe = dployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -19,6 +20,11 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
